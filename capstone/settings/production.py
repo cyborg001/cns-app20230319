9ac3cad6c59f95e3bf44ae5de0f6a87089a5f22c
cyborg001 <cgrs27@gmail.com>
2023-03-19 15:15:04 -0400

    Modifica los datos de la base de datos y el json secrets.json

--- capstone/settings/production.py
+++ capstone/settings/production.py
@@ -1,25 +1,34 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost','127.0.0.1','cnsapp','www.cnsapp']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','cnsapp','www.cnsapp','10.0.0.182']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
 DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': get_secret("DATABASE_NAME"),
+            'USER': get_secret("DATABASE_USER"),
+            'PASSWORD': get_secret("DATABASE_PASSWORD"),
+            'HOST':get_secret("DATABASE_HOST"),
+            'PORT':get_secret("DATABASE_PORT")
+    },
     #     'default': {
     #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #         'NAME': get_secret("DATABASE_NAME"),
-    #         'USER': get_secret("DATABASE_USER"),
-    #         'PASSWORD': get_secret("DATABASE_PASSWORD"),
-    #         'HOST': 'localhost',
+    #         'NAME':"postgres",
+    #         'USER': "postgres",
+    #         'PASSWORD': "ANALISISUASD",
+    #         'HOST':"10.0.0.182",
+    #         'PORT':"5432"
     # },
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 }
-
+# print(SECRET_KEY)
 # import dj_database_url  
 # db_from_env = dj_database_url.config(conn_max_age=500)  
 # DATABASES['default'].update(db_from_env)
